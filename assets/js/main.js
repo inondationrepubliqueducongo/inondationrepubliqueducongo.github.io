a37c3bdbb0c7136b973424399287247347697daf
Maksim Grishkin <FFFFF@bk.ru>
2024-08-22 23:16:03 +0300

    feat: Добавлено наполнение главной страницы

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,14 +22,3 @@ window.onscroll = function() {
 }
 
 scrollFunction();
-
-//****************** Карта
-
-var map = L.map('map').setView([-4.1995, 15.2135], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
